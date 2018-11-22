8809f483ef914d2c4e103c0d0389958ce5e0479f
KWH <arangojulian93@gmail.com>
2018-11-21 20:04:26 -0500

    'NewGame'

---
+++ Stateds/state1.js
@@ -0,0 +1,17 @@
+var demo = {};
+demo.state1 = function(){};
+demo.state1.prototype = {
+  preload: function(){
+   
+  },
+  create: function(){
+   
+    game.stage.backgroundColor = '#800080';
+  
+  },
+  update: function(){
+
+}
+};
+
+
